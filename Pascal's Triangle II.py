55a68a5424285c61a2d22062bf7687af213fc919
Birook2023 <122909706+Birook2023@users.noreply.github.com>
2023-03-15 12:20:29 +0300

    Create Pascal's Triangle II.py

---
+++ Pascal's Triangle II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        
+        res = [1] * (rowIndex+1)
+
+        if rowIndex == 0:
+            return res
+        
+        prev_row = self.getRow(rowIndex-1)
+        
+        for i in range(1, len(res)-1):
+            res[i] = prev_row[i-1] + prev_row[i]
+        
+        return res
