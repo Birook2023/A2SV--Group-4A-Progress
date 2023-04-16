8efd4aa8b970e0641c848b0d6f91da3b4981c419
Birook2023 <122909706+Birook2023@users.noreply.github.com>
2023-04-16 08:58:35 +0300

    Update Reverse String.py

--- Reverse String.py
+++ Reverse String.py
@@ -3,12 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        # Using recursion method
-        def Reverse(l, r):
-            if l >= r:
-                return 
-            lis = s[l]
-            s[l], s[r] = s[r] , lis
-            
-            return Reverse(l+1, r-1)
-        return Reverse(0, len(s)-1)
+        i, j = 0, len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        return s
